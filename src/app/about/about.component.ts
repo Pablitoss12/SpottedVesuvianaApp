1c2851b8a2533cffc82fddccb8a75ae8d2246543
merlino <paolocozzolinopipicelli@gmail.com>
2020-05-20 12:38:12 +0200

    About component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  user: any[] ;
+  constructor(httpclient: HttpClient) {
+    setTimeout(() => {
+      httpclient.get<any[]>('https://jsonplaceholder.typicode.com/users').subscribe(result => this.user = result);
+    }, 2000);
+  }
 
   ngOnInit() {}
 
